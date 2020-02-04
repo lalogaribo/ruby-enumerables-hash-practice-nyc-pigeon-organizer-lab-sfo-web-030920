b66db060add6f01526e6ac7ec5b328582d1b9575
Eduardo mendoza <garibo.eduardo90@gmail.com>
2020-02-04 22:18:29 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,7 +1,7 @@
 
 
 # pigeon_data.each do |key, value|
-#   puts "#{key} ---- #{value}"
+#   puts "#{key} ----> #{value}"
 #     value.each do |new_val, names|
 #     puts "#{new_val} ---- #{names}"
 #       names.each do |name|
